a0a8416984564210c0d5fd75cd5f87a43b75b28b
Cathy-0111 <79830971+Cathy-0111@users.noreply.github.com>
2021-10-31 20:01:59 +0800

    Update 1013-1.c

--- 1013-1.c
+++ 1013-1.c
@@ -1,24 +1,17 @@
+#include<stdio.h>
 #include<float.h>
-#include <stdio.h>
 
-int main()
-{
-    float n=-1;
-    float max=-FLT_MAX,min=FLT_MAX;
-    for(float i =0 ; i < 10;i++ )
-    {
+int main(){
+    float n,min=FLT_MAX,max=-FLT_MAX;
+    for(int i=0;i<10;i++){
         scanf("%f",&n);
-        if (max<n)
-        {
+        if(max<n){
             max=n;
         }
-
-        if(min>n)
-        {
+        if(min>n){
             min=n;
         }
-        
     }
-    printf("max: %.2f\n",(float)max);
-    printf("min: %.2f\n",(float)min);
+    printf("max: %.2f\n",max);
+    printf("min: %.2f\n",min);
 }
